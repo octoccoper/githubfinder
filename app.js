7a145c50115c0db98c4062821510a4094bb88831
octoccoper <octocopper@gmail.com>
2019-01-12 22:12:51 +0200

    started add fucntions to file app.js and github.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const searchUser = document.getElementById("searchUser");
+
+searchUser.addEventListener("keyup", (e) => {
+        const userText = e.target.value;
+
+        if (!userText) {
+
+        }
+    }
+
+)
