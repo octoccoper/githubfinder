5bd260b1931289a1dd8ef2efd0791555f1880fa2
octoccoper <octocopper@gmail.com>
2019-01-13 12:48:20 +0200

    updated app.js and github.js for make an http call to github server

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+// Initialiaze Github class
+const github = new Github;
+
+// input for search user
 const searchUser = document.getElementById("searchUser");
 
+let profileData = document.getElementById("profile");
+
+// check data that user enter in the input
 searchUser.addEventListener("keyup", (e) => {
         const userText = e.target.value;
 
-        if (!userText) {
+        if (!userText == "") {
 
+            // Make HTTP call
+            github.getUser(userText)
+            .then( data => {
+                if(data.profile.message === "Not Found") {
+                    profileData.innerHTML = "This user doesn't exist";
+                } else {
+                    profileData.innerHTML = JSON.stringify(data);
+                }
+            } )
         }
     }
 
--- github.js
+++ github.js
@@ -1,5 +1,15 @@
 class Github {
     constructor() {
-        this.client_id="";
+        this.client_id = "0eb39d26063ebc34694c";
+        this.client_secret = "090e795b717f5195903180999b4b26fac3871bed";
     }
+
+    async getUser(user) {
+        const profilerResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}?client_secret=${this.client_secret}`);
+        const profile = await profilerResponse.json();
+        return {
+            profile
+        }
+    }
+
 }
